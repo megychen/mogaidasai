48da67e7b6c8359728524123a69d5039f3139d71
megy <megy@megys-MacBook-Pro.local>
2017-01-12 12:17:32 +0800

    implement job CRUD

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,49 @@
+class JobsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to root_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    flash[:alert] = "Job deleted"
+    redirect_to root_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
