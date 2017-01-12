9ba6b815c80ef4b4210d66e40fb1fcb696080f5b
megy <megy@megys-MacBook-Pro.local>
2017-01-12 14:02:55 +0800

    add sidebar & hide/publish action for admin panel

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,10 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end
   end
 
   def index
