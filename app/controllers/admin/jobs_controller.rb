8320e4239ad93618fb400eb8c766e6c3f21c3e8d
megy <megy@megys-MacBook-Pro.local>
2017-01-12 14:43:07 +0800

    implement filter by salaray

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -8,7 +8,14 @@ class Admin::JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.all
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.recent
+            end
   end
 
   def new
--- app/models/job.rb
+++ app/models/job.rb
@@ -5,6 +5,7 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, numericality: { greater_than: 0 }
 
   scope :recent, -> { order("created_at DESC") }
+  scope :published, -> { where(is_hidden: false) }
 
   def publish!
     self.is_hidden = false
