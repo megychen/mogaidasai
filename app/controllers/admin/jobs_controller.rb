c2cbbba753aed9b68a9b57e1308cf06140558809
megy <megy@megys-MacBook-Pro.local>
2017-01-13 13:23:02 +0800

    fix syntax error

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -8,14 +8,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def index
-    @jobs = case params[:order]
-            when 'by_lower_bound'
-              Job.published.order('wage_lower_bound DESC')
-            when 'by_upper_bound'
-              Job.published.order('wage_upper_bound DESC')
-            else
-              Job.published.recent
-            end
+    @jobs = Job.all
   end
 
   def new
