dff53c6acbcc1b2bb14c3cbebd5ed90b4d74df75
megy <megy@megys-MacBook-Pro.local>
2017-01-20 10:02:57 +0800

    change login redirect to current page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   def require_is_admin
     if !current_user.admin?
       flash[:alert] = "You are not admin"
       redirect_to root_path
     end
+  end
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_url
   end
 end
