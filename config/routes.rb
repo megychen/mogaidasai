2c636891f4511b23196f274610df26644c6f1cb6
megy <megy@megys-MacBook-Pro.local>
2017-01-27 13:55:11 +0800

    distinct candidate and employer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :user
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :jobs do
     collection do
       get :search
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119141224) do
+ActiveRecord::Schema.define(version: 20170127030556) do
 
   create_table "chat_rooms", force: :cascade do |t|
     t.string   "title"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20170119141224) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
+    t.boolean  "is_candidate",           default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
